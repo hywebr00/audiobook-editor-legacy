8ba3337c297b4d54a534178d7c0051ab7b812488
cobra.yu@hyweb.com.tw <>
2021-09-25 12:57:33 +0800

    1. Update version number.

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ DISPLAY_ON_SCREEN = 1
 ORGANIZATION_NAME = "Hyweb Technology CO., LTD."
 ORGANIZATION_DOMAIN = "hyweb.com.tw"
 APPLICATION_NAME = "Audiobook Editor"
-APPLICATION_VERSION = "0.7.4.0"
+APPLICATION_VERSION = "0.7.4.1"
 
 
 class Controller(QObject):
