2fe112e6fcad4514d35c4f57d5c1c86648ca13c2
cobra.yu@hyweb.com.tw <>
2021-11-08 20:56:12 +0800

    Modified Runme script

--- runme(macOS).sh
+++ runme(macOS).sh
@@ -4,8 +4,6 @@ pyinstaller -F -n AEditor \
 --add-data="Language/appLang_EN.qm:Language" \
 --add-data="svg/icon:svg/icon" \
 --add-data="Noto_Sans_TC:Noto_Sans_TC" \
---add-data="Noto_Sans:Noto_Sans" \
 --add-data="audiobooks.schema.json:." \
---add-data="about_licenses.html:." \
 --windowed --noconfirm \
 main.py
