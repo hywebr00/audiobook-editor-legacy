0c72e8e4ccc2e10f813cc075663f3df01c4448ae
cobra.yu@hyweb.com.tw <>
2021-11-18 23:16:25 +0800

    1.Use 'appdmg' instead of 'DMG canvas' 2.Add info.plist to support HiDPI display

--- runme(macOS).sh
+++ runme(macOS).sh
@@ -1,10 +1,11 @@
-pyinstaller -F -n AEditor \
+pyinstaller -w -D -n AEditor \
 -i AudiobookEditorLogo.icns \
---add-data="Language/appLang_TC.qm:Language" \
 --add-data="Language/appLang_EN.qm:Language" \
+--add-data="Language/appLang_TC.qm:Language" \
+--add-data="Language/qt_en.qm:Language" \
+--add-data="Language/qt_zh_TW.qm:Language" \
 --add-data="svg/icon:svg/icon" \
 --add-data="Noto_Sans_TC:Noto_Sans_TC" \
 --add-data="Noto_Sans:Noto_Sans" \
 --add-data="audiobooks.schema.json:." \
---windowed --noconfirm \
 main.py
